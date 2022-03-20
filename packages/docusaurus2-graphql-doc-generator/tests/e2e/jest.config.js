81dc7600d4fe1e791bdd22bbecde67f7654ff83f
Grégory Heitz <edno@edno.io>
2022-03-20 18:51:08 +0100

    Delete packages/docusaurus2-graphql-doc-generator directory (#15)
    * Delete packages/docusaurus2-graphql-doc-generator directory
    
    * Update README.md

--- packages/docusaurus2-graphql-doc-generator/tests/e2e/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  collectCoverage: false,
-  displayName: "End-to-End Tests",
-  name: "e2e",
-  rootDir: __dirname,
-  testEnvironment: "node",
-  testMatch: ["<rootDir>/**/?(*.)+(spec|test).js"],
-  verbose: true,
-};
